63e42e9064d18da1f9e182bcc85f1956aacbadfc
dracoooooo <191250037@smail.nju.edu.cn>
2021-01-21 21:35:32 +0800

    使用相似度矩阵聚类

--- src/clustering/cluster.py
+++ src/clustering/cluster.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import SpectralClustering
@@ -8,12 +9,11 @@ from sklearn.metrics import silhouette_score
 from src.text_based_similarities.Levenshtein_distance import Levenshtein_similarity
 from src.text_based_similarities.cos_similarity import cos_similarity
 from src.text_based_similarities.jaccard_coefficient import Jaccard_cofficient
+from src.text_based_similarities.Sorensen_Dice_coefficient import Sorensen_Dice_coefficient
 from src.token_based_similarities.token_lcs import token_lcs
 from src.languange_classifier.code_language_classifier import suffix
 
 # 使用谱聚类算法(spectral clustering)通过代码相似度对代码分类
-
-# data_path = "../data/leetcode/raw"
 
 
 def file_paths(data_path, language):
@@ -28,7 +28,7 @@ def file_paths(data_path, language):
 
 
 def compute_similarity_matrix(similarity_function):
-    path = "../data/leetcode/raw"
+    path = "../../data/leetcode/raw"
     files = file_paths(path, "java")
     len = files.__len__()
     matrix = np.empty([len, len])
@@ -36,21 +36,29 @@ def compute_similarity_matrix(similarity_function):
         for j in range(len):
             matrix[i][j] = similarity_function(files[i], files[j])
             print(i, j)
+
+    f = open('similarity_matrix_' + similarity_function.__name__ + '.pkl', 'wb')
+    pickle.dump(matrix, f)
+    f.close()
     return matrix
 
 
-def test():
-    # SpectralClustering(n_clusters=8,
-    #                    eigen_solver='arpack',
-    #                    n_init=10,
-    #                    gamma=1.0,
-    #                    affinity='rbf',
-    #                    n_neighbors=10)
-    return 1
+def initSC(similarity_matrix_path):
+    sc = SpectralClustering(n_clusters=8, affinity='precomputed', assign_labels='discretize')
+    f1 = open(similarity_matrix_path, 'rb')
+    similarity_matrix = pickle.load(f1)
+    f1.close()
+    print(similarity_matrix)
+    sc.fit_predict(similarity_matrix)
+    return sc
+
 
 if __name__ == "__main__":
-    sc = SpectralClustering(n_clusters=2, affinity='precomputed',
-                                            assign_labels='discretize')
-    similarity_matrix = compute_similarity_matrix(Jaccard_cofficient)
-    print(similarity_matrix)
-    # sc.fit_predict(similarity_matrix)
+    path = "../../data/leetcode/raw"
+    files = file_paths(path, "java")
+    sc = initSC('./similarity_matrix_Jaccard_cofficient.pkl')
+    labels = sc.labels_
+    dic = {}
+    for i in range(files.__len__()):
+        dic[files[i]] = labels[i]
+    a = 1
