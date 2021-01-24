33b61c6d700f0bd8c4190b05f71618778404f595
Czj <1301215701@qq.com>
2021-01-24 17:56:10 +0800

    try clustering

--- src/clustering/cluster.py
+++ src/clustering/cluster.py
@@ -6,6 +6,7 @@ from sklearn.cluster import SpectralClustering
 from sklearn.preprocessing import StandardScaler, normalize
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
+from sklearn import metrics
 from src.text_based_similarities.Levenshtein_distance import Levenshtein_similarity
 from src.text_based_similarities.cos_similarity import cos_similarity
 from src.text_based_similarities.jaccard_coefficient import Jaccard_cofficient
@@ -33,8 +34,8 @@ def compute_similarity_matrix(similarity_function):
     len = files.__len__()
     matrix = np.empty([len, len])
     for i in range(len):
-        for j in range(len):
-            matrix[i][j] = similarity_function(files[i], files[j])
+        for j in range(i,len):
+            matrix[j][i] = matrix[i][j] = similarity_function(files[i], files[j])
             print(i, j)
 
     f = open('similarity_matrix_' + similarity_function.__name__ + '.pkl', 'wb')
@@ -44,12 +45,17 @@ def compute_similarity_matrix(similarity_function):
 
 
 def initSC(similarity_matrix_path):
-    sc = SpectralClustering(n_clusters=8, affinity='precomputed', assign_labels='discretize')
+    sc = SpectralClustering(n_clusters=3, affinity='precomputed', assign_labels='discretize')
     f1 = open(similarity_matrix_path, 'rb')
     similarity_matrix = pickle.load(f1)
     f1.close()
     print(similarity_matrix)
-    sc.fit_predict(similarity_matrix)
+    plt.scatter(similarity_matrix[:,0], similarity_matrix[:,1], marker = 'o')
+    plt.show()
+    y_pred = sc.fit_predict(similarity_matrix)
+    # print("Calinski-Harabasz Score", metrics.calinski_harabaz_score(similarity_matrix, y_pred))
+    plt.scatter(similarity_matrix[:,2], similarity_matrix[:,1], c = y_pred)
+    plt.show()
     return sc
 
 
@@ -58,8 +64,10 @@ if __name__ == "__main__":
     files = file_paths(path, "java")
     sc = initSC('./similarity_matrix_Jaccard_cofficient.pkl')
     labels = sc.labels_
-    # dic = {}
-    # for i in range(files.__len__()):
-    #     dic[files[i]] = labels[i]
-    score = silhouette_score(files, labels)
-    print(score)
+    dic = {}
+    for i in range(files.__len__()):
+        dic[files[i]] = labels[i]
+    for i in dic.items():
+        print(i)
+    # score = silhouette_score(files, labels)
+    # print(score)
